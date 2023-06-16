882e378d794591ffa31402e8b8eb892ba37551eb
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-16 13:25:27 +0530

    Updates

---
+++ precache-manifest.6e5083d384a73a4451bd68b6ece9bc37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9497079aaf5adea41d25",
+    "url": "/color-picker/static/css/main.7f2ebf4c.chunk.css"
+  },
+  {
+    "revision": "9497079aaf5adea41d25",
+    "url": "/color-picker/static/js/main.9497079a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/color-picker/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/color-picker/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "53709cf3e73b6675887e",
+    "url": "/color-picker/static/js/runtime~main.53709cf3.js"
+  },
+  {
+    "revision": "772a0eb643ff790ffc49eaad6bee9a30",
+    "url": "/color-picker/index.html"
+  }
+];
